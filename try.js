c989f3bc5b75b00b7b4134efdf1f7134a8dff160
Saswati Patra and Uriel Gonzalez <uriel@>
2019-07-01 12:11:44 -0700

    try-catch test

---
+++ try.js
@@ -0,0 +1,29 @@
+$(document).ready(function() {
+  $('#submittedNumber').click(function() {
+    // debugger;
+    const inputtedNumber = parseInt($('#number').val());
+    $('#number').val("");
+
+    function checkNumber(number) {
+      if (isNaN(number) || number < 0) {
+        return new Error("Not a valid number!");
+      } else {
+        return true;
+      }
+    }
+
+    try {
+      const isNumberValid = checkNumber(inputtedNumber);
+      if (isNumberValid instanceof Error) {
+        console.error(isNumberValid.message);
+        $('#displayNumber').text("This number is invalid. Try again.")
+        throw RangeError("Not a valid number!");
+      } else {
+        console.log("Try was successful, so no need to catch!");
+        $('#displayNumber').text("This number is valid. You may continue.")
+      }
+    } catch(error) {
+      console.error(`Red alert! We have an error: ${error.message}`)
+    }
+  });
+});
